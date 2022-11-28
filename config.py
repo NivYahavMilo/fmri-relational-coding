da7354ad715ef63608f2831e03397a71998bef76
NivYahavMilo <nivyahav.ny@gmail.com>
2022-11-28 11:51:52 +0200

    voxel level extraction out of raw data

--- config.py
+++ config.py
@@ -2,3 +2,11 @@ import os
 
 ROOT_PATH = os.path.abspath(os.path.curdir)
 DATA_CENTER = os.path.join(ROOT_PATH, 'data_center')
+
+RAW_DATA_TASK = os.path.join(r'E:', 'S1200', '7T_task')
+RAW_DATA_REST = os.path.join(r'E:', 'S1200', '7T_rest')
+DATA_DRIVE_E = os.path.join(r'E:', 'parcelled_data_niv')
+VOXEL_DATA = os.path.join(DATA_DRIVE_E, 'schaefer2018_VOXEL_TASK')
+VOXEL_DATA_DF = os.path.join(DATA_DRIVE_E, 'schaefer2018_VOXEL_TASK_DF')
+
+GRAYORIDNATES = 59412
---
+++ data_normalizer/utils.py
@@ -0,0 +1,58 @@
+import os
+
+import numpy as np
+import pandas as pd
+
+import config
+
+
+def _info(s: str):
+    print('-' * 10)
+    print(s)
+    print('-' * 10)
+
+
+def _get_parcel(roi, net=7):
+    '''
+    return:
+    parcel: grayordinate -> ROI map
+    nw_info: subnetwork tags for each ROI
+    '''
+    parcel_path = os.path.join(config.DATA_DRIVE_E, 'cifti',
+                               'Schaefer2018_%dParcels_%dNetworks_order.csv' % (roi, net))
+
+    df = pd.read_csv(parcel_path)
+    parcel = np.array(df['ROI'])
+
+    info_path = parcel_path.replace('.csv', '_info_condensed.csv')
+    df = pd.read_csv(info_path)
+    nw_info = np.array(df['network'])
+
+    return parcel, nw_info
+
+
+def _get_clip_labels(timing_file, k_runs: int = 4):
+    '''
+    assign all clips within runs a label
+    use 0 for testretest
+    '''
+
+    clips = []
+    for run in range(k_runs):
+        run_name = 'MOVIE%d' % (run + 1)  # MOVIEx_7T_yz
+        timing_df = timing_file[timing_file['run'].str.contains(run_name)]
+        timing_df = timing_df.reset_index(drop=True)
+
+        for jj, row in timing_df.iterrows():
+            clips.append(row['clip_name'])
+
+    clip_y = {}
+    jj = 1
+    for clip in clips:
+        if 'testretest' in clip:
+            clip_y[clip] = 0
+        else:
+            clip_y[clip] = jj
+            jj += 1
+
+    return clip_y
