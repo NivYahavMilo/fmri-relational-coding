e8bfa24ceb3996b6ed89b685f3e033d72b2fc5a6
Niv Yahav <niv.yahav@stargo.co>
2022-11-30 14:23:42 +0200

    FmriRelationalCoding

--- relational_coding/fmri_relationl_coding.py
+++ relational_coding/fmri_relationl_coding.py
@@ -1,13 +1,23 @@
+from enums import Mode
 from relational_coding.relational_coding_base import RelationalCodingBase
+
 
 class FmriRelationalCoding(RelationalCodingBase):
 
+    def single_tr_correlation(self, timepoint):
+        pass
+
+    def rest_between_iteration(self):
+        rest_tr_gen = self.rest_between_tr_generator()
+        _tr = next(rest_tr_gen, None)
+        while _tr:
+            self.single_tr_correlation(timepoint=_tr)
+
     def run(self, roi):
-        subject_gen = self.yield_subject_iterator()
-        sub_id = next(subject_gen,None)
+        subject_gen = self.yield_subject_generator()
+        sub_id = next(subject_gen, None)
         while sub_id:
-            roi_sub_data = self.load_roi_data(roi_name=roi,subject=sub_id)
-
-
+            roi_sub_data_task = self.load_roi_data(roi_name=roi, subject=sub_id, mode=Mode.CLIPS)
+            roi_sub_data_rest = self.load_roi_data(roi_name=roi, subject=sub_id, mode=Mode.REST)
 
 
--- relational_coding/relational_coding_base.py
+++ relational_coding/relational_coding_base.py
@@ -6,25 +6,21 @@ from data_center.static_data.static_data import StaticData
 from enums import Mode
 from abc import abstractmethod
 
+
 class RelationalCodingBase:
 
-    mode: Mode
-
-    @abstractmethod
-    def run(self, roi: str):
-        """
-        run relational coding flow.
-        """
-        pass
+    @staticmethod
+    def rest_between_tr_generator():
+        yield [*range(0,20)]
 
     @staticmethod
-    def yield_subject_iterator():
+    def yield_subject_generator():
         yield StaticData.SUBJECTS
 
-    def load_roi_data(self, roi_name: str, subject: str):
+    def load_roi_data(self, roi_name: str, subject: str, mode: Mode):
         self._check_roi_validity(roi_name)
 
-        data_path = config.SUBNET_DATA_DF.format(mode=self.mode.value)
+        data_path = config.SUBNET_DATA_DF.format(mode=mode.value)
         roi_data_p = os.path.join(data_path, subject, roi_name)
         roi_data = open(roi_data_p, 'rb')
         roi_data_df = pickle.load(roi_data)
@@ -36,4 +32,11 @@ class RelationalCodingBase:
     @staticmethod
     def _check_roi_validity(roi_name: str):
         if roi_name not in StaticData.ROI_NAMES:
-            raise ValueError("ROI name incorrect\n","check the following list:\n", StaticData.ROI_NAMES)
+            raise ValueError("ROI name incorrect\n", "check the following list:\n", StaticData.ROI_NAMES)
+
+    @abstractmethod
+    def run(self, roi: str):
+        """
+        run relational coding flow.
+        """
+        pass
